93b0ea5b07a83f9a9845a9d016000135d8bb8cc3
PRUTHVIRAJ2002 <119094580+PRUTHVIRAJ2002@users.noreply.github.com>
2026-01-12 11:21:12 +0530

    Update stopwords.py

--- app/utils/stopwords.py
+++ app/utils/stopwords.py
@@ -1,14 +1,12 @@
 STOP_WORDS = set([
-    "i","me","my","myself","we","our","ours","ourselves","you","your","yours",
-    "yourself","yourselves","he","him","his","himself","she","her","hers",
-    "herself","it","its","itself","they","them","their","theirs","themselves",
-    "what","which","who","whom","this","that","these","those","am","is","are",
-    "was","were","be","been","being","have","has","had","having","do","does",
-    "did","doing","a","an","the","and","but","if","or","because","as","until",
-    "while","of","at","by","for","with","about","against","between","into",
-    "through","during","before","after","above","below","to","from","up","down",
-    "in","out","on","off","over","under","again","further","then","once","here",
-    "there","when","where","why","how","all","any","both","each","few","more",
-    "most","other","some","such","no","nor","not","only","own","same","so",
-    "than","too","very","s","t","can","will","just","don","should","now"
+    "i","me","my","myself","we","our","ours","you","your","yours",
+    "he","him","his","she","her","hers","it","its","they","them",
+    "what","which","who","this","that","is","are","was","were",
+    "be","been","being","have","has","had","do","does","did",
+    "a","an","the","and","but","if","or","because","as","of",
+    "at","by","for","with","about","against","between","into",
+    "through","before","after","to","from","in","out","on","off",
+    "over","under","again","then","once","here","there","when",
+    "where","why","how","all","any","few","more","most","other",
+    "some","no","nor","not","only","same","so","than","too","very"
 ])
