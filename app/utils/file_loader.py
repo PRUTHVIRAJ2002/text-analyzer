c2f6d5bb908419dbc97c7f31b78688e2b3b3e6c3
PRUTHVIRAJ2002 <119094580+PRUTHVIRAJ2002@users.noreply.github.com>
2026-01-12 12:47:07 +0530

    Update file_loader.py

--- app/utils/file_loader.py
+++ app/utils/file_loader.py
@@ -1,13 +1,16 @@
 import os
-from fastapi import HTTPException
 
-BASE_DIR = "data"
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = /app/app
+
+PROJECT_ROOT = os.path.dirname(BASE_DIR)
+# PROJECT_ROOT = /app
 
 def load_text(file_path: str) -> str:
-    full_path = os.path.join(BASE_DIR, file_path)
+    full_path = os.path.join(PROJECT_ROOT, file_path)
 
     if not os.path.exists(full_path):
-        raise HTTPException(status_code=404, detail="File not found")
+        raise FileNotFoundError(f"File not found: {full_path}")
 
     with open(full_path, "r", encoding="utf-8") as f:
         return f.read()
