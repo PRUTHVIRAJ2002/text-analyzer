b38042bf5a8af476f9573adbe9957be9acfaf5f3
PRUTHVIRAJ2002 <119094580+PRUTHVIRAJ2002@users.noreply.github.com>
2026-01-12 11:21:55 +0530

    Update file_loader.py

--- app/utils/file_loader.py
+++ app/utils/file_loader.py
@@ -1,4 +1,13 @@
-def load_text(file_path: str) -> str:
-    with open(file_path, "r", encoding="utf-8") as f:
-        return f.read()
+import os
+from fastapi import HTTPException
 
+BASE_DIR = "data"
+
+def load_text(file_path: str) -> str:
+    full_path = os.path.join(BASE_DIR, file_path)
+
+    if not os.path.exists(full_path):
+        raise HTTPException(status_code=404, detail="File not found")
+
+    with open(full_path, "r", encoding="utf-8") as f:
+        return f.read()
